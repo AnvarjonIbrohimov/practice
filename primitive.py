fd4a4d5c1f0c49227d80639f74b04f4e58bfe564
Anvarjon <menhozirkareyadamanmail.ru@gmail.com>
2026-04-03 21:40:04 +0900

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -18,3 +18,23 @@ print(f"counter value: {counter}, counter type: {counter_type}")
 result = counter.bit_count()
 result2 = counter.numerator
 print(result, result2)
+
+print("================ string =======================")
+# METHODS: upper(), lower(), title(), split(), join(), replace(), find()
+
+course = "AI python fullstack"
+result3 = type(course)
+print(f"the type of course is: {result3}")
+result4 = course.title()     # faqat bosh harflarini katta harfga aylantiradi
+print(f"the title of the course is: {result4}")
+
+result5 = course.lower()     # hamma harflarni kichik harfga aylantiradi
+result6 = course.upper()     # hamma harflarni katta harfga aylantiradi 
+print(f"the lower case of the course is: {result5}")
+print(f"the upper case of the course is: {result6}")
+result6 = course.replace("python", "java")  # string ichidagi so'zni boshqa so'zga almashtiradi
+print(f"the replaced course is: {result6}")
+result7 = course.split()    # stringni bo'sh joy bo'yicha bo'lib, ro'yxatga aylantiradi
+print(f"the split course is: {result7}")
+
+print("================ boolean =======================")
