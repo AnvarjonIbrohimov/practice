74f9f3da69095e7177cf98118d1c0abeae425c5a
Anvarjon <menhozirkareyadamanmail.ru@gmail.com>
2026-04-17 19:03:42 +0900

    fix: python specia;/magic methods

--- class.about.py
+++ class.about.py
@@ -2,8 +2,11 @@
 CLASS
     (1) What is class?
     (2) ordinary vs static properties
-    (3) special methods
+    (3) special/magic methods
 '''
+
+
+
 
 print("============================= What is class?  =================================")
 # classlar obect yasab beruvchi shablon 
@@ -15,6 +18,7 @@ class Person ():
     message = "class state property"
 
     # constructor 
+
     def __init__(self, name, age):  # self bu Person dan yaratilgan object hisoblanadi == js dagi (this)
         self.name = name   # this.name = name
         self.age = age      # this.age = age
@@ -29,6 +33,8 @@ class Person ():
     def explain(cls):
         print("Class: static method property executed!")
 
+
+    
 person1 = Person("Jasica", 23)
 person2 = Person("Jamshid", 27)
 person3 = Person("Sattor", 21)
@@ -48,15 +54,45 @@ print("new_message:", new_message)
 # static method 
 Person.explain()
 
+print("============================= special methods  =================================")
+# special/magic methodlarni "python magic method rszalski > https://rszalski.github.io/magicmethods/" shu yerda korsak boladi 
+# __init__  __new__ __str__ __call__ __getitem__ __eq__ __len__ ...
+class Car():
+    # state
+    description = "This class makes car"
+
+    
+    def __new__(cls, *args):
+        print("*__new__")
+        return super().__new__(cls)
+    
+    def __init__(self, name, year):
+      self.name = name
+      self.year = year
+
+    #method
+    def start_engine(self):
+        print(f"the {self.name} started engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):
+        return f"{self.name} was produced in {self.year} year!"
+
+    def __call__(self):
+      print(f"Object called as function!")
+      return True
+
+my_car = Car("tOYOTA", 2024)
+my_car.start_engine()
+my_car.stop_engine()
 
 
-
-
-
-
-
-
-
-
+print("-----------------")
+your_car = Car("Ferrari", 2025)
+print(your_car)
+response = your_car()  # call
+print("response: ", response)
 
 
